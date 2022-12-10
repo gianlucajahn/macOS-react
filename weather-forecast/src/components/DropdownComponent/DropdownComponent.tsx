3a87b673ff8f8842237c8b1a4ee96297449fda75
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-12-10 03:55:25 +0100

    Add dropdown menu's to NavBar

--- weather-forecast/src/components/DropdownComponent/DropdownComponent.tsx
+++ weather-forecast/src/components/DropdownComponent/DropdownComponent.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import getDropdownContent from '../../utils/keys/helpers/getDropdownContent';
+import { store } from '../../App';
+import './DropdownComponent.scss';
 
 export default function DropdownComponent () {
+  const [state, dispatch] = useContext(store)
+  const content = getDropdownContent(state);
+
   return (
-    <div className="dropdown">
-      
+    <div className="dropdown-menu">
+      {content.map((item, i) => {
+        if (item.name === "divider") {
+            return (
+                <div className="divider" />
+            )
+        }
+
+        return (
+            <div className="dropdown-item" style={{ color: item.available ? "white" : "#ccc" }}>
+                {item.name}
+            </div>
+        )
+      })}
     </div>
   );
 }
--- weather-forecast/src/components/NavBar/NavBar.tsx
+++ weather-forecast/src/components/NavBar/NavBar.tsx
@@ -3,6 +3,7 @@ import { store } from '../../App';
 import './NavBar.scss';
 import { ReactComponent as Settings} from "../../resources/images/svg/settings.svg";
 import getDate from '../../utils/keys/helpers/getDate';
+import DropdownComponent from '../DropdownComponent/DropdownComponent';
 
 const date = getDate();
 
@@ -24,40 +25,48 @@ export default function () {
             <div className="logo section" id="logo" onClick={dispatchAction}>
                 <img className="apple" src={require("../../resources/images/apple.png")} />
                 <div className={`${state.section === "logo" ? "selected-logo" : "not-selected"}`}></div>
+                {state.section === "logo" ? <DropdownComponent /> : null} 
             </div>
     
             <div className="finder section" id="finder" onClick={dispatchAction}>
                 Finder
                 <div className={`${state.section === "finder" ? "selected-finder" : "not-selected"}`}></div>
+                {state.section === "finder" ? <DropdownComponent /> : null} 
             </div>
     
             <div className="file section" id="file" onClick={dispatchAction}>
                 File
                 <div className={`${state.section === "file" ? "selected-file" : "not-selected"}`}></div>
+                {state.section === "file" ? <DropdownComponent /> : null} 
             </div>
     
             <div className="edit section" id="edit" onClick={dispatchAction}>
                 Edit
                 <div className={`${state.section === "edit" ? "selected-edit" : "not-selected"}`}></div>
+                {state.section === "edit" ? <DropdownComponent /> : null} 
             </div>
     
             <div className="view section" id="view" onClick={dispatchAction}>
                 View
                 <div className={`${state.section === "view" ? "selected-view" : "not-selected"}`}></div>
+                {state.section === "view" ? <DropdownComponent /> : null} 
             </div>
             <div className="go section" id="go" onClick={dispatchAction}>
                 Go
                 <div className={`${state.section === "go" ? "selected-go" : "not-selected"}`}></div>
+                {state.section === "go" ? <DropdownComponent /> : null} 
             </div>
     
             <div className="windows section" id="windows" onClick={dispatchAction}>
                 Window
                 <div className={`${state.section === "windows" ? "selected-window" : "not-selected"}`}></div>
+                {state.section === "windows" ? <DropdownComponent /> : null} 
             </div>
     
             <div className="help section" id="help" onClick={dispatchAction}>
                 Help
                 <div className={`${state.section === "help" ? "selected-help" : "not-selected"}`}></div>
+                {state.section === "help" ? <DropdownComponent /> : null} 
             </div>
 
             <div className="right">
