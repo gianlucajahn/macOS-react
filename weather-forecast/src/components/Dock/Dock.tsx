d1b2f198acad75442c95784c12e23d4e273cee41
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-12-11 23:08:06 +0100

    Add link indicators to DockItems

--- weather-forecast/src/components/Dock/Dock.tsx
+++ weather-forecast/src/components/Dock/Dock.tsx
@@ -5,20 +5,44 @@ export default function Dock () {
   return (
     <>
         <div className="dock">
-            <img className="dock-icon finder" src={require("../../resources/images/webp/finder.png")} />
+            <div className="dock-item">
+              <img className="dock-icon finder" src={require("../../resources/images/webp/finder.png")} />
+              <div className="point" />
+            </div>
             <div className="division" />
-            <img className="dock-icon" src={require("../../resources/images/webp/photos.png")} />
-            <img className="dock-icon" src={require("../../resources/images/webp/calculator.png")} />
-            <img className="dock-icon" src={require("../../resources/images/webp/calendar.png")} />
-            <img className="dock-icon" src={require("../../resources/images/webp/vscode.png")} />
-            <img className="dock-icon" src={require("../../resources/images/webp/applemusic.png")} />
+            <div className="dock-item no-point">
+              <img className="dock-icon" src={require("../../resources/images/webp/photos.png")} />
+            </div>
+            <div className="dock-item no-point">
+              <img className="dock-icon" src={require("../../resources/images/webp/calculator.png")} />
+            </div>
+            <div className="dock-item no-point">
+              <img className="dock-icon" src={require("../../resources/images/webp/calendar.png")} />
+            </div>
+            <div className="dock-item no-point">
+              <img className="dock-icon" src={require("../../resources/images/webp/vscode.png")} />
+            </div>
+            <div className="dock-item no-point">
+              <img className="dock-icon" src={require("../../resources/images/webp/applemusic.png")} />
+            </div>
             <div className="division" />
-            <img className="dock-icon" src={require("../../resources/images/webp/weather.png")} />
-            <img className="dock-icon" src={require("../../resources/images/webp/github.png")} />
+            <div className="dock-item no-point">
+              <img className="dock-icon" src={require("../../resources/images/webp/weather.png")} />
+            </div>
+            <div className="dock-item">
+              <img className="dock-icon" src={require("../../resources/images/webp/github.png")} />
+              <div className="point" />
+            </div>
             <div className="division" />
-            <img className="dock-icon" src={require("../../resources/images/webp/scalable.png")} />
+            <div className="dock-item">
+              <img className="dock-icon" src={require("../../resources/images/webp/scalable.png")} />
+              <div className="point" />
+            </div>
             <div className="division" />
-            <img className="dock-icon" src={require("../../resources/images/webp/twitter.png")} />
+            <div className="dock-item">
+              <img className="dock-icon" src={require("../../resources/images/webp/twitter.png")} />
+              <div className="point" />
+            </div>
         </div>
     </>
   );
