cbd2b713e5047ac0140531e97de150c5e860120a
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-12-12 01:56:36 +0100

    Add links to corresponding items in Dock

--- weather-forecast/src/components/Dock/Dock.tsx
+++ weather-forecast/src/components/Dock/Dock.tsx
@@ -42,11 +42,11 @@ export default function Dock () {
               <div className="tool-tip">Calendar</div>
               <img className="dock-icon" src={require("../../resources/images/webp/calendar.png")} />
             </div>
-            <div className={`dock-item no-point ${state.dockItem === 4 ? "hovered" : ""} ${state.dockItem === 2 ? "distance-2" : state.dockItem === 3 ? "distance-1" : state.dockItem === 5 ? "distance-1" : state.dockItem === 6 ? "distance-2" : ""}`} id="4" onMouseEnter={selectDockItem} onMouseLeave={resetDock}>
+            <div className={`dock-item no-point ${state.dockItem === 4 ? "hovered" : ""} ${state.dockItem === 2 ? "distance-2" : state.dockItem === 3 ? "distance-1" : state.dockItem === 5 ? "distance-1" : state.dockItem === 6 ? "distance-2" : ""}`} id="4" onMouseEnter={selectDockItem} onMouseLeave={resetDock} onClick={()=> window.open("https://code.visualstudio.com/", "_blank")}>
               <div className="tool-tip">VSCode</div>
               <img className="dock-icon" src={require("../../resources/images/webp/vscode.png")} />
             </div>
-            <div className={`dock-item no-point ${state.dockItem === 5 ? "hovered" : ""} ${state.dockItem === 3 ? "distance-2" : state.dockItem === 4 ? "distance-1" : state.dockItem === 6 ? "distance-1" : state.dockItem === 7 ? "distance-2" : ""}`} id="5" onMouseEnter={selectDockItem} onMouseLeave={resetDock}>
+            <div className={`dock-item no-point ${state.dockItem === 5 ? "hovered" : ""} ${state.dockItem === 3 ? "distance-2" : state.dockItem === 4 ? "distance-1" : state.dockItem === 6 ? "distance-1" : state.dockItem === 7 ? "distance-2" : ""}`} id="5" onMouseEnter={selectDockItem} onMouseLeave={resetDock} onClick={()=> window.open("https://www.apple.com/de/apple-music/", "_blank")}>
               <div className="tool-tip">Music</div>
               <img className="dock-icon" src={require("../../resources/images/webp/applemusic.png")} />
             </div>
@@ -56,19 +56,19 @@ export default function Dock () {
               <img className="dock-icon" src={require("../../resources/images/webp/weather.png")} />
               <div className="point" style={{ visibility: document.getElementById('weather-window')?.classList.contains('window') ? "visible" : "hidden" }} />
             </div>
-            <div className={`dock-item ${state.dockItem === 7 ? "hovered" : ""} ${state.dockItem === 5 ? "distance-2" : state.dockItem === 6 ? "distance-1" : state.dockItem === 8 ? "distance-1" : state.dockItem === 9 ? "distance-2" : ""}`} id="7" onMouseEnter={selectDockItem} onMouseLeave={resetDock}>
+            <div className={`dock-item ${state.dockItem === 7 ? "hovered" : ""} ${state.dockItem === 5 ? "distance-2" : state.dockItem === 6 ? "distance-1" : state.dockItem === 8 ? "distance-1" : state.dockItem === 9 ? "distance-2" : ""}`} id="7" onMouseEnter={selectDockItem} onMouseLeave={resetDock} onClick={()=> window.open("https://github.com/gianlucajahn/macOS-react", "_blank")}>
               <div className="tool-tip">GitHub</div>
               <img className="dock-icon" src={require("../../resources/images/webp/github.png")} />
               <div className="point" />
             </div>
             <div className="division" />
-            <div className={`dock-item ${state.dockItem === 8 ? "hovered" : ""} ${state.dockItem === 6 ? "distance-2" : state.dockItem === 7 ? "distance-1" : state.dockItem === 9 ? "distance-1" : ""}`} id="8" onMouseEnter={selectDockItem} onMouseLeave={resetDock}>
+            <div className={`dock-item ${state.dockItem === 8 ? "hovered" : ""} ${state.dockItem === 6 ? "distance-2" : state.dockItem === 7 ? "distance-1" : state.dockItem === 9 ? "distance-1" : ""}`} id="8" onMouseEnter={selectDockItem} onMouseLeave={resetDock} onClick={()=> window.open("https://de.scalable.capital/", "_blank")}>
               <div className="tool-tip">Scalable</div>
               <img className="dock-icon" src={require("../../resources/images/webp/scalable.png")} />
               <div className="point" />
             </div>
             <div className="division" />
-            <div className={`dock-item ${state.dockItem === 9 ? "hovered" : ""} ${state.dockItem === 7 ? "distance-2" : state.dockItem === 8 ? "distance-1" : ""}`} id="9" onMouseEnter={selectDockItem} onMouseLeave={resetDock}>
+            <div className={`dock-item ${state.dockItem === 9 ? "hovered" : ""} ${state.dockItem === 7 ? "distance-2" : state.dockItem === 8 ? "distance-1" : ""}`} id="9" onMouseEnter={selectDockItem} onMouseLeave={resetDock} onClick={()=> window.open("https://twitter.com/Apple", "_blank")}>
               <div className="tool-tip">Twitter</div>
               <img className="dock-icon" src={require("../../resources/images/webp/twitter.png")} />
               <div className="point" />
